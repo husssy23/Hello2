6e2188e0c1ce981645c2349887485eef46a86901
Hussain Morawala <hussainmorawala@gmail.com>
2017-03-10 03:08:05 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/ads'
+
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+  get 'login' => :new
+  post 'login' => :create
+  delete 'logout' => :destroy
+  end
+
+  get 'admin/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  resources :sessions
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
@@ -6,7 +28,6 @@ Rails.application.routes.draw do
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "store#index"
-  
-
+  #root 'products#index'
+  root 'store#index'
 end
